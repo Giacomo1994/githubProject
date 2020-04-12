1fead852bb38441a44812bb2ae3051efbca64838
Giacomo1994 <giacomopeppebotto@gmail.com>
2020-04-12 13:24:13 +0200

    commit 2

--- app/src/main/java/com/example/githubproject/MainActivity.java
+++ app/src/main/java/com/example/githubproject/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //github commit!
+        //github commit 2!
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
