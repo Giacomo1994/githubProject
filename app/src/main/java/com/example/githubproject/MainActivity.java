1d70278946c49da4deb5b7fe5c62a707f087a59d
Giacomo1994 <giacomopeppebotto@gmail.com>
2020-04-12 13:18:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/githubproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.githubproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //github created
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
